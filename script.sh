b0bd9e4979b0ca123aac40167ab493679bbbea88
rishabharora1228 <114339911+rishabharora1228@users.noreply.github.com>
2023-02-01 13:59:46 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+sudo apt -y update
+sudo apt install openjdk-18-jre-headless -y
+sudo apt  install golang-go -y
+git clone https://github.com/opstree/OT-Microservices.git
+echo "###############################################################
+#                        OT-Microservices                     #
+###############################################################
+#                  Welcome to OT-Microservices                #
+#       All connections are monitored and recorded.           #
+#  Disconnect IMMEDIATELY if you are not an authorized user!  #
+###############################################################
+" > welcome
+cat /home/ubuntu/welcome | sudo tee -a /etc/issue.net > /dev/null
+echo 'Banner /etc/issue.net' | sudo tee -a /etc/ssh/sshd_config > /dev/null
+sudo systemctl restart ssh.service
